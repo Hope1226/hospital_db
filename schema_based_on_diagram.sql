c4e1cc5962a5675bbf1c8b3719ece965b79ec2b7
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-19 21:55:20 +0500

    Create a table treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,9 @@ CREATE TABLE IF NOT EXISTS patients(
     name VARCHAR(255),
     date_of_birth DATE
 );
+
+CREATE TABLE IF NOT EXISTS treatments(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    type VARCHAR(255),
+    name VARCHAR(255)
+);
