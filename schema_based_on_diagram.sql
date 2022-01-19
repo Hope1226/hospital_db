2298598fb7143d949063f94ce244fc7a3bdaaa77
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-19 22:14:28 +0500

    Create table medical histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -2,11 +2,24 @@
 CREATE TABLE IF NOT EXISTS patients(
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
-    date_of_birth DATE
+    date_of_birth DATE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS treatments(
     id INT GENERATED ALWAYS AS IDENTITY,
     type VARCHAR(255),
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS medical_histories(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    anmited_at TIMESTAMP,
+    patient_id INT,
+    status VARCHAR(50),
+    PRIMARY KEY (id),
+    CONSTRAINT fk_patients_medical_histories
+        FOREIGN KEY(patient_id)
+            REFERENCES patients(id)   
 );
