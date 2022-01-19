442e24e40fcb6a5369ab3eb91f186d6d20855538
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-19 22:28:19 +0500

    Create a table medical_histories_treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -23,3 +23,16 @@ CREATE TABLE IF NOT EXISTS medical_histories(
         FOREIGN KEY(patient_id)
             REFERENCES patients(id)   
 );
+
+CREATE TABLE IF NOT EXISTS medical_histories_treatments (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    med_history_id INT,
+    treatment_id INT,
+    CONSTRAINT fk_medical_histories_medical_histories_treatments
+        FOREIGN KEY(med_history_id)
+            REFERENCES medical_histories(id),
+    CONSTRAINT fk_treatments_medical_histories_treatments
+        FOREIGN KEY(treatment_id)
+            REFERENCES treatments(id)
+);
+
